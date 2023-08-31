55c8fbee888b8ae23c65c4a59808d4aefc8f31fe
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-08-31 12:00:32 -0300

    Recuperando dados dos produtos

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,7 +1,27 @@
+import { useParams } from "react-router-dom";
+import { ListaProdutos } from "../components/ListaProdutos";
+
 export default function EditarProdutos() {
-    return(
-        <>
-            <h1>Editar Produtos</h1>
-        </>
-    )
+  //Recuperando o id que chega no request através do PATH.
+  //Vamos urtilizar o Hook useParams().
+
+  const {id} = useParams();
+
+  const produtoRecuperado = ListaProdutos.filter((produto) => {
+     if(produto.id == id){
+        return produto;
+     }
+  });
+
+  const objProdutoRecuperado = produtoRecuperado[0];
+
+  return (
+    <>
+      <h1>EditarProdutos</h1>
+      <h2>PRODUTO</h2>
+      <p>NOME : {objProdutoRecuperado.nome}</p>
+      <p>DESCRIÇÃO : {objProdutoRecuperado.desc}</p>
+      <p>PREÇO : {objProdutoRecuperado.preco}</p>
+    </>
+  );
 }
