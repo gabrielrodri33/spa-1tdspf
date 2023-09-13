46e662fd737dcdfc0136c88e05e30fd84cbaaa57
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-09-13 11:44:20 -0300

    sincronizado com professor

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -10,9 +10,6 @@ export default function Cabecalho(){
                     <ul>
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/produtos">Produtos</Link></li>
-                        <li><Link to="/editar/produtos/1">Produto - 1</Link></li>
-                        <li><Link to="/editar/produtos/2">Produto - 2</Link></li>
-                        <li><Link to="/editar/produtos/3">Produto - 3</Link></li>
                     </ul>
                 </nav>
             </header>
