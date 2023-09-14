80983af2a19e3bfc651612e7c204bde969a67eec
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-09-14 10:54:45 -0300

    info de produto recuperada em excluir

--- src/routes/ExcluirProdutos.jsx
+++ src/routes/ExcluirProdutos.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useNavigate, useParams } from "react-router-dom";
+import { ListaProdutos } from "../components/ListaProdutos";
 
 export default function ExcluirProdutos() {
+
+  document.title = "EXCLUIR PRODUTO";
+
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const produto = ListaProdutos.filter((produto) => produto.id == id)[0];
+
+  
+
+
   return (
     <>
-        <div>ExcluirProdutos</div>
+        <div>
+          <h1>Excluir Produtos</h1>
+          
+          <div>
+            <h2>Produto selecionado</h2>
+            <figure>
+              <img src={produto.img} alt={produto.desc} />
+              <figcaption>{produto.nome} - <span>R$ {produto.preco}</span></figcaption>
+            </figure>
+          </div>
+
+        </div>
     </>
     
   )
