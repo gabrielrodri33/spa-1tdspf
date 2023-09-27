368f1b0ddfba4d9d3b6f9435fd5576f6d30786bd
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-09-27 12:18:09 -0300

    modal inserir

--- src/components/ModalInserir/ModalInserir.jsx
+++ src/components/ModalInserir/ModalInserir.jsx
@@ -1,14 +1,32 @@
-import styles from "./ModalInserir.module.css"
+import styles from "./ModalInserir.module.css";
 
 export default function ModalInserir(props) {
 
-    if (props.open) {
-       return (
-         <div className={styles.container}>
-           <h1>Modal Inserir</h1>
-           <button onClick={()=> props.setOpen(false)}>CLOSE-MODAL</button>
-         </div>
-       );
-     }
-   }
-   
+    document.title = "CADASTRO";
+
+    let novoId;
+
+    fetch("http://localhost:5000/produtos",{
+        method:"GET",
+        headers:{
+            "Content-Type":"application/json"
+        }
+    })
+    .then((response)=> response.json())
+    .then((data)=>{
+        console.log(data.length)
+        novoId = (data[data.length-1].id+1)
+        })
+    .catch(error=> console.log(error));
+
+    console.log("NOVO ID " + novoId);
+
+ if (props.open) {
+    return (
+      <div className={styles.container}>
+        <h1>Modal Inserir</h1>
+        <button onClick={()=> props.setOpen(false)}>CLOSE-MODAL</button>
+      </div>
+    );
+  }
+}
